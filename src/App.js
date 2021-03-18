e8153345877517ba875ef423093b79421f466daf
vgrana <valeriagrandi@hotmail.com>
2021-03-18 15:17:42 -0300

    probando titulo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
        
           <header>
             <nav class="navbar">
-           
+           <h1> A ver si aparece..</h1>
               <NavEmpresa />
             </nav>
           </header>
